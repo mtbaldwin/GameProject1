648726b36f9d65a94fbe25e157332206a5ce66da
Matt Baldwin <Matt Baldwin>
2016-05-13 19:32:43 -0400

    Buttons notification

---
+++ ClickerGame1/Assets/scripts/ChestNote.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChestNote : MonoBehaviour {
+	public GameObject note;
+	public bool active;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		note.SetActive(active);
+}
+}
