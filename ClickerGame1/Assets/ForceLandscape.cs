6c3a76185412398bca1af64be3e18f6945ec9531
Matt Baldwin <Matt Baldwin>
2016-05-12 21:56:12 -0400

    Initial

---
+++ ClickerGame1/Assets/ForceLandscape.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceLandscape : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Screen.orientation = ScreenOrientation.AutoRotation;
+        Screen.autorotateToLandscapeLeft = Screen.autorotateToLandscapeRight = true;
+        Screen.autorotateToPortrait = Screen.autorotateToPortraitUpsideDown = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ ClickerGame1/Assets/click.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class click : MonoBehaviour{
+	public static int count =0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown (){
+		count += 1;
+		Debug.Log (count);
+	}
+}
